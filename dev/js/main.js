92a36d0aa9be48e4420136530fd97a3760f878a5
taksenov <taksenov@gmail.com>
2014-10-10 21:20:15 +0600

    Верстка сделана, примерно на 70 процентов.

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,43 @@
+//todo окультурить этлот код!
+
+$(function() {
+/*    var spinner = $('#spinner').spinner(*/
+    var spinner = $('#spinner1').spinner(
+        {
+            min: 0,
+            max: 10,
+            spin: function(event, ui) {
+                $(this).change();
+            }
+        }
+    );
+    var spinner = $('#spinner2').spinner(
+        {
+            min: 0,
+            max: 10,
+            spin: function(event, ui) {
+                $(this).change();
+            }
+        }
+    );
+    value = 0;
+
+    $( "#setvalue2" ).click(
+        function() {
+            var value = spinner.spinner( "value" );
+            value = value + 1;
+            spinner.spinner( "value", value  );
+        }
+    );
+
+    $( "#setvalue3" ).click(
+        function() {
+            var value = spinner.spinner( "value" );
+            value = value - 1;
+            spinner.spinner( "value", value );
+        }
+    );
+
+    $( "button" ).button();
+});
 
